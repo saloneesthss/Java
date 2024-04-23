42d4369f3acf58b2629ed4efc003ea3910144c62
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-04-23 14:00:29 +0545

    Added Java lab reports

---
+++ Lab-17 Thread Priority 26/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        TestMultiPriority1 m1=new TestMultiPriority1();
+        TestMultiPriority1 m2=new TestMultiPriority1();
+        m1.setPriority(Thread.MIN_PRIORITY);
+        m2.setPriority(Thread.MAX_PRIORITY);
+        m1.start();
+        m2.start();
+    }
+}
+class TestMultiPriority1 extends Thread {
+    public void run() {
+        System.out.println(Thread.currentThread().getPriority());
+    }
+}
